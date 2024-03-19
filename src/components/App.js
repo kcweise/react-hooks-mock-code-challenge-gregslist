932df1d30f83d6709e133917cac5ef9c01f08b9f
Keenan Weise <kcweise@gmail.com>
2024-03-19 11:31:38 -0500

    All core deliverables done

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,13 @@ import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
-  const [listings, setListings] = useState([])
+  const [listings, setListings] = useState([]);
+  const [filteredListings, setFilteredListings] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
   const url = "http://localhost:6001/listings"
 
   useEffect(()=> {
+
   fetch(url)
     .then (res => {
       if (res.ok) {
@@ -15,15 +18,30 @@ function App() {
       throw new Error("Network response was not ok");
     })
     .then (data => setListings(data))
+    .then (data => setFilteredListings(data))
     .catch(error => console.error("Error", error))
   },[])
 
-console.log (listings)
+ useEffect(() =>{
+    setFilteredListings(listings);
+  }, [listings]);
+
+console.log(listings)
+console.log (filteredListings)
+
+const handleSearch = (query) => {
+  setSearchQuery(query);
+  const filtered = listings.filter(listing => 
+    listing.description.toLowerCase().includes(query.toLowerCase())
+  );
+  setFilteredListings(filtered);
+  console.log(filtered)
+};
 
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer listings = {listings} />
+      <Header onSearch = {handleSearch} searchQuery = {searchQuery}/>
+      <ListingsContainer listings = {filteredListings} />
     </div>
   );
 }
